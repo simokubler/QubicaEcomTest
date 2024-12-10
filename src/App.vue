a9c20b269e51df93eb9c1e03ca971440bac8feab
simokubler <simone.kubler@gmail.com>
2024-12-10 12:01:11 +0100

    App.vue - add BaseHeader component + <router-view> tag for views

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const isShowSideBar = ref(false);
+
+const handleSidebar = (showSideBar) => {
+  isShowSideBar.value = showSideBar;
+};
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <BaseHeader @show-sidebar="handleSidebar"/>
+    <router-view />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
