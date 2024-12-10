28deaca623df3f6ee932650fa59ef8d510a684e7
simokubler <simone.kubler@gmail.com>
2024-12-10 18:28:04 +0100

    WishList.vue - finalized logic structure + add router-link in HeaderButtonWishList

--- src/components/HeaderButtonWishList.vue
+++ src/components/HeaderButtonWishList.vue
@@ -2,18 +2,20 @@
 import { ref, computed } from 'vue';
 import { useLocalStorage } from '@vueuse/core';
 
-const wishItems = useLocalStorage<{ idprod: number; title: string;}[]>('wishItems', []);
+const wishItems = useLocalStorage<{ idprod: number; title: string; image: string; priceSing: number}[]>('wishItems', []);
 const wishItemsCount = computed<number>(() => wishItems.value.length);
 
 </script>
 
 <template>
-    <div class="header__wishlist">
+    <router-link 
+        :to="{name: 'WishList'}"
+        class="header__wishlist">
         <span class="header__wishlist-icon">
             <img src="../assets/img/wishlist_add.svg"/>
         </span>
         <span class="header__wishlist-count">{{ wishItemsCount }}</span>
-    </div>
+    </router-link>
 </template>
 
 <style></style>
--- src/views/WishList.vue
+++ src/views/WishList.vue
@@ -4,39 +4,17 @@ import { useLocalStorage } from '@vueuse/core';
 import { useRouter } from 'vue-router';
 
 const router = useRouter();
-// const RouteParamsId = route.params.id as string;
-// const productId = Number(RouteParamsId);
-// const product = ref<{ 
-//     id: number; 
-//     title: string; 
-//     price: number; 
-//     description: string;
-//     category: string;
-//     image: string;
-//     rating: object
-//     } | null>(null);
 const isLoggedIn = useLocalStorage<boolean>('isLoggedIn', false);
 const cartItems = useLocalStorage<{ idprod: number; title: string; image: string; qty: number; priceSing: number, priceTot: number }[]>('cartItems', []);
-const wishItems = useLocalStorage<{ idprod: number; title: string;}[]>('wishItems', []);
-const isLoadingProd = ref<boolean>(true);
+const wishItems = useLocalStorage<{ idprod: number; title: string; image: string; priceSing: number}[]>('wishItems', []);
 // const prodQty = ref<number>(1);
 
 const checkIsLogged = ():void => {
     if(!isLoggedIn.value) router.push({name: 'Home'})
 }
 
-const updateItem = (idProd: number, qtyProd: number, priceProd: number) => {
-    const existingIndex = cartItems.value.findIndex(item => item.idprod === idProd);
-    if (existingIndex !== -1) {
-        cartItems.value[existingIndex] = {
-            ...cartItems.value[existingIndex],
-            qty: qtyProd,
-            priceTot: qtyProd * priceProd,
-        };
-    }
-}
 const removeItem = (idProd: number) => {
-    cartItems.value = cartItems.value.filter(item => item.idprod !== idProd); 
+    wishItems.value = wishItems.value.filter(item => item.idprod !== idProd); 
 }
 
 watch(
@@ -46,14 +24,6 @@ watch(
   }
 );
 
-const totalCart = computed<number>(() => {
-    let total = 0;
-    for (const item of cartItems.value) {
-        total += item.priceTot;
-    }
-    return total;
-});
-
 onMounted(() => {
     checkIsLogged();
 });
@@ -62,46 +32,34 @@ onMounted(() => {
 <template>
     <div>
         <div>
-            <h2>cartItems</h2>
-            {{ cartItems }}
+            <h2>wishItems</h2>
+            {{ wishItems }}
         
-            <div v-if="cartItems.length > 0" class="cart_container">
-                <div class="cart_item">
-                    <div class="cart_item-img">Immagine</div>
-                    <div class="cart_item-title">Titolo</div>
-                    <div class="cart_item-qty">Quantità</div>
-                    <div class="cart_item-price">Prezzo</div>
+            <div v-if="wishItems.length > 0" class="wishlist_container">
+                <div class="wish_item">
+                    <div class="wish_item-img">Immagine</div>
+                    <div class="wish_item-title">Titolo</div>
+                    <div class="wish_item-price">Prezzo</div>
                 </div>
-                <div v-for="item in cartItems" :key="item.idprod" class="cart_item">
-                    <div class="cart_item-img">
+                <div v-for="item in wishItems" :key="item.idprod" class="wish_item">
+                    <div class="wish_item-img">
                         <img :src="item.image" />
                     </div>
-                    <div class="cart_item-title">
+                    <div class="wish_item-title">
                         {{ item.title }}
                     </div>
-                    <div class="cart_item-qty">
-                        <input 
-                        type="number"
-                        min="1"
-                        v-model="item.qty"
-                        @input="updateItem(item.idprod, item.qty, item.priceSing)"
-                        />
+                    <div class="wish_item-price">
+                        &euro; {{ item.priceSing }}
                     </div>
-                    <div class="cart_item-price">
-                        &euro; {{ item.priceTot }}
-                    </div>
-                    <div class="cart_item-delete pointer">
+                    <div class="wish_item-delete pointer">
                         <img 
                         @click="removeItem(item.idprod)"
                         src="../assets/img/delete.svg" />
                     </div>
                 </div>
-                <div class="total_container">
-                    <div class="total-item">&euro; {{ totalCart }}</div>
-                </div>
             </div>
             <div v-else>
-                <i>Nessun prodotto nel carrello</i>
+                <i>Nessun prodotto nella lista dei desideri</i>
             </div>
         </div>
     </div>
