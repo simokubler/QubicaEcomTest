d8376a3122e436479edeeb59181b3ded2da27610
simokubler <simone.kubler@gmail.com>
2024-12-11 18:32:12 +0100

    HeaderButton<component> - add count animation

--- src/components/HeaderButtonCart.vue
+++ src/components/HeaderButtonCart.vue
@@ -1,21 +1,30 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { useLocalStorage } from '@vueuse/core';
 
 const cartItems = useLocalStorage<{ idprod: number; title: string; image: string; qty: number; priceSing: number, priceTot: number }[]>('cartItems', []);
 const cartItemsCount = computed<number>(() => cartItems.value.length);
+const cartCount = ref<HTMLElement | null>(null);
 
+watch(cartItemsCount, (newVal, oldVal) => {
+  if (cartCount.value) {
+    cartCount.value.classList.add('bounce');
+    setTimeout(() => {
+      cartCount.value?.classList.remove('bounce');
+    }, 300); 
+  }
+});
 </script>
 
 <template>
     <router-link
-    :to="{ name: 'Cart' }"
-    class="header__cart pointer"
-    >
+        :to="{ name: 'Cart' }"
+        class="header__cart"
+        >
         <button class="header__cart-icon">
             <img src="../assets/img/cart_logo.svg"/>
         </button>
-        <span class="header__cart-count">{{ cartItemsCount }}</span>
+        <span ref="cartCount" class="header__cart-count">{{ cartItemsCount }}</span>
     </router-link>
 </template>
 
--- src/components/HeaderButtonWishList.vue
+++ src/components/HeaderButtonWishList.vue
@@ -1,10 +1,19 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { useLocalStorage } from '@vueuse/core';
 
 const wishItems = useLocalStorage<{ idprod: number; title: string; image: string; priceSing: number}[]>('wishItems', []);
 const wishItemsCount = computed<number>(() => wishItems.value.length);
+const wishlistCount = ref<HTMLElement | null>(null);
 
+watch(wishItemsCount, (newVal, oldVal) => {
+  if (wishlistCount.value) {
+    wishlistCount.value.classList.add('bounce');
+    setTimeout(() => {
+      wishlistCount.value?.classList.remove('bounce');
+    }, 300); 
+  }
+});
 </script>
 
 <template>
@@ -14,7 +23,7 @@ const wishItemsCount = computed<number>(() => wishItems.value.length);
         <button class="header__wishlist-icon">
             <img src="../assets/img/wishlist_add.svg"/>
         </button>
-        <span class="header__wishlist-count">{{ wishItemsCount }}</span>
+        <span ref="wishlistCount" class="header__wishlist-count">{{ wishItemsCount }}</span>
     </router-link>
 </template>
 
